2d5c55d8fa0d75dbe0ccbbe5103640b5793b4d21
uitanmaciel <uitan.s.maciel@gmail.com>
2024-08-29 17:46:59 -0300

    :sparkles: feat: Included Web Project

---
+++ src/DevSource.Stack.Web/IEndpoint.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace DevSource.Stack.Web;
+
+public interface IEndpoint
+{
+    static abstract void Map(IEndpointRouteBuilder app);
+}
---
+++ src/DevSource.Stack.Web/IRequestToCommand.cs
@@ -0,0 +1,9 @@
+﻿using DevSource.Stack.Core;
+
+namespace DevSource.Stack.Web;
+
+public interface IRequestToCommand<TRequest, TCommand> where TCommand : ICommand
+{
+    static abstract TCommand ToCommand(TRequest request);
+    static abstract IList<TCommand> ToCommand(IList<TRequest> requests);
+}
